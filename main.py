c3ec1261c25f058c5146009201f44f523198698c
FoXeN <121698405+FoXeNe@users.noreply.github.com>
2024-06-25 02:09:18 +0300

    Easy flask and template added

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import requests
 
+from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
 
-first = "рубль"
-second = "гривны"
+app = Flask(__name__)
 
-url = f"https://www.google.com/search?q=конвертер+валют+{first}+{second}&sca_esv=d9eef79a7f076b3b&sxsrf=ADLYWIIC9WZeQqd3YV-IiULmXlNKoG2K6g%3A1719267042482&ei=4u55ZqCPHZKA1fIPooeziA0&oq=%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%D0%B5%D1%80+%D0%B2%D0%B0%D0%BB%D1%8E%D1%82&gs_lp=Egxnd3Mtd2l6LXNlcnAiHdC60L7QvdCy0LXRgNGC0LXRgCDQstCw0LvRjtGCKgIIATIHECMYsAMYJzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIZEC4YgAQYsAMY0QMYQxjHARjIAxiKBdgBATIZEC4YgAQYsAMY0QMYQxjHARjIAxiKBdgBATIWEC4YgAQYsAMYQxjUAhjIAxiKBdgBAUjxFFAAWABwAXgBkAEAmAEAoAEAqgEAuAEDyAEAmAIBoAIImAMAiAYBkAYMugYECAEYCJIHATGgBwA&sclient=gws-wiz-serp"
+@app.route('/')
+def converter():
+    first = "рубль"
+    second = "гривны"
 
-# хиадер делает вид, что ты человек, что бы можно было сделать запрос. #!Без него не работает
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+    url = f"https://www.google.com/search?q=конвертер+валют+{first}+{second}&sca_esv=d9eef79a7f076b3b&sxsrf=ADLYWIIC9WZeQqd3YV-IiULmXlNKoG2K6g%3A1719267042482&ei=4u55ZqCPHZKA1fIPooeziA0&oq=%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%D0%B5%D1%80+%D0%B2%D0%B0%D0%BB%D1%8E%D1%82&gs_lp=Egxnd3Mtd2l6LXNlcnAiHdC60L7QvdCy0LXRgNGC0LXRgCDQstCw0LvRjtGCKgIIATIHECMYsAMYJzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIZEC4YgAQYsAMY0QMYQxjHARjIAxiKBdgBATIZEC4YgAQYsAMY0QMYQxjHARjIAxiKBdgBATIWEC4YgAQYsAMYQxjUAhjIAxiKBdgBAUjxFFAAWABwAXgBkAEAmAEAoAEAqgEAuAEDyAEAmAIBoAIImAMAiAYBkAYMugYECAEYCJIHATGgBwA&sclient=gws-wiz-serp"
 
-# получаем страницу с конвертором валют
-fullPage = requests.get(url, headers=headers)
+    # хиадер делает вид, что ты человек, что бы можно было сделать запрос. #!Без него не работает
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
 
-# парсим содержимое страницы
-soup = BeautifulSoup(fullPage.content, 'html.parser')
+    # получаем страницу с конвертором валют
+    fullPage = requests.get(url, headers=headers)
 
-# находим и выводим цифру конвертации
-convert = soup.find("span", {"class": "DFlfde SwHCTb", "data-precision":"2"})
+    # парсим содержимое страницы
+    soup = BeautifulSoup(fullPage.content, 'html.parser')
 
-print(convert.text)
+    # находим и выводим цифру конвертации
+    convert = soup.find("span", {"class": "DFlfde SwHCTb", "data-precision":"2"})
+
+    return render_template('index.html', convert=convert.text)
+app.run(debug=True)

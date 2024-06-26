918dc4b2f789750ab1f3227a688741e7e6a76045
FoXeN <121698405+FoXeNe@users.noreply.github.com>
2024-06-27 01:09:56 +0300

    handlers added

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import logging
 import sys
 import os
 
-
 from values import CurrencyConverter
 from aiogram import Bot, Dispatcher
+import aiogram.utils.markdown as fmt
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,6 +24,14 @@ currency = CurrencyConverter()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+    await message.answer("Что бы увидеть все команды напишите /help")
+
+@dp.message(Command("help"))
+async def help_handler(message: Message) -> None:
+    await message.answer("Что бы увидеть курс валют напишите /check \nЧто бы увидеть все команды напишите /help")
+
+@dp.message(Command("check"))
+async def rate_handler(message: Message) -> None:
     curr = currency.check_currency_rate()
     await message.answer(f"Текущий курс валют: {curr}")
 
